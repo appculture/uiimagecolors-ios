f12fe608341e15d8e38a86585e9c8cf4797a7f54
Marko Tadic <marko.tadic@appculture.com>
2016-06-03 17:11:08 +0200

    Implemented initial logic for taking screenshots

--- GlobusUITests/GlobusUITests.swift
+++ GlobusUITests/GlobusUITests.swift
@@ -30,7 +30,32 @@ class GlobusUITests: XCTestCase {
     // MARK: - Screenshots
     
     func testTakeScreenshots() {
-        snapshot("0-CustomerCard")
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        let tabBarsQuery = app.tabBars
+        
+        /// globus card
+        sleep(1)
+        snapshot("0-CustomerCard", waitForLoadingIndicator: false)
+        
+        /// account
+        tablesQuery.staticTexts["My account"].tap()
+        sleep(1)
+        snapshot("1-AccountSettings", waitForLoadingIndicator: false)
+        
+        /// vouchers
+        tabBarsQuery.buttons["Vouchers"].tap()
+        sleep(2)
+        tablesQuery.cells.containingType(.StaticText, identifier:"20% Rabatt auf alle JOHN FRIEDA Produkte").staticTexts["13.06.2016"].tap()
+        sleep(1)
+        snapshot("2-Voucher", waitForLoadingIndicator: false)
+        
+        /// branches
+        tabBarsQuery.buttons["Branches"].tap()
+        sleep(1)
+        tablesQuery.cells.staticTexts["GLOBUS Westside"].tap()
+        sleep(1)
+        snapshot("3-Branch", waitForLoadingIndicator: false)
     }
     
 }
