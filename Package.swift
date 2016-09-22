f1ff8507d88787ff16470e8f6ad1bc2c2af1f4db
Igor Vojinovic <igor.vojinovic@appculture.com>
2016-09-22 14:31:33 +0200

    Updated Package.swift Fixed crash when requesting camera.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Globus",
     dependencies: [
-        .Package(url: "https://oprandi@bitbucket.org/appculture/uiimagecolors-ios.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/appculture/uiimagecolors-ios.git", majorVersion: 0, minor: 2)
     ]
 )
 
